7ce5ccb9e17afe78de3b3dff211c06e32c1bdcad
louisdrf <louis.driffort@gmail.com>
2023-06-05 13:18:06 +0200

    fix bug collisions monstres

--- cfiles/monster/move_monster.c
+++ cfiles/monster/move_monster.c
@@ -56,10 +56,10 @@ void get_collision_monster(Game *g, Monster *m, Map *map, int direction, int dis
 
 // on recupere les coordonnées des cases adjacentes dans la map de collisions
 
-    int caseGauche = map->quadmap[m->positionY][m->positionX - 1];
-    int caseDroite = map->quadmap[m->positionY][(m->positionX + (m->rect.w/TILESIZE)) + 1];
-    int caseHaut = map->quadmap[m->positionY - 1][m->positionX];
-    int caseBas = map->quadmap[m->positionY + 1][m->positionX];
+    int caseGauche = map->quadmap[m->positionY + ((m->rect.h/TILESIZE) / 2)][m->positionX - 1];
+    int caseDroite = map->quadmap[m->positionY + ((m->rect.h/TILESIZE) / 2)][(m->positionX + (m->rect.w/TILESIZE))];
+    int caseHaut = map->quadmap[m->positionY - 1][m->positionX + ((m->rect.w/TILESIZE) / 2)];
+    int caseBas = map->quadmap[m->positionY + (m->rect.h/TILESIZE) + 1][m->positionX + ((m->rect.w/TILESIZE) / 2)];
 
 //
     switch(direction) {
@@ -69,7 +69,7 @@ void get_collision_monster(Game *g, Monster *m, Map *map, int direction, int dis
         break;
 
         case RIGHT:
-        if(caseGauche == 0) make_move_monster(m, map, g, direction, p);
+        if(caseDroite == 0) make_move_monster(m, map, g, direction, p);
         break;
 
         case UP:
